dcb994eba32fba459ba3d46e0cd6ddd7da34ea66
Cagatay Gunay <cagataygunay91@gmail.com>
2021-11-23 23:23:42 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
+
+def get_topic_page(topic, page_number=1):
+    topic_with_page = topic + '/page=' + str(page_number)
+    response = requests.get(topic)
+    # Writing the parsed contents into a .html file, inorder to see if all the required data is visible when .html
+    # document is viewed
+    # with open('Cryptocurrency.html', 'w') as f:
+    #     f.write(response.text)
+
+    if not response.ok:
+        print('Status code:', response.status_code)
+        raise Exception('Failed to fetch web page' + topic)
+
+    return BeautifulSoup(response.text, features="html.parser")
+
+
+# get the date array first for the last year
+day_count = 365
+dates = []
+for i in range(day_count, 0, -1):
+    dates.append((date.today() + relativedelta(days=-day_count)).strftime('%Y%m%d'))
+
+# historical daturl format: https://coinmarketcap.com/historical/20210103/
+
+url_base = "https://coinmarketcap.com/historical/"
+url = url_base+dates[0]+'/'
+content = requests.get(url).content
+soup = BeautifulSoup(content,'html.parser')
+# table = soup.find('table', {'class': 'table'})
+doc = get_topic_page(url)
+
+# tr_head = doc.thead.find_all('tr')
+tr_tags = doc.tbody.find_all('tr')
+td_tags = tr_tags[0].find_all('td')
+print(td_tags[0].div.text)   # 0: rank 2: symbol, 3: market cap
+# tr_tags indices give the nth crypto
+# td_tags indices give the info about the selected crypto
+
+
+
+Cagatay = 1
